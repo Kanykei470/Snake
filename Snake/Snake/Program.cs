bc562a62e8a6aebeeba02b1e46cae1c6a9317c26
Kanykei <akanykej73@gmail.com>
2021-07-15 04:54:49 +0600

    Snake can exist with food

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,33 +11,57 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(91,22);
+            //Set console properties
+            Console.SetWindowSize(91,22);   
             Console.SetBufferSize(91,22);
             Console.CursorVisible = false;
-            
+
+            //Create lines for frame 
             HorizontalLine upline = new HorizontalLine(0,89,0, '-');
             HorizontalLine downline = new HorizontalLine(0,89,20, '-');
             VerticalLine leftline = new VerticalLine(0,20,0, '|');
             VerticalLine rightline = new VerticalLine(0,20,89, '|');
+
+            //Draw frame
             upline.draw();
             downline.draw();
             leftline.draw();
             rightline.draw();
-           
+
+            //Create lines and draw snake
             Point p1 = new Point(4, 5, '*');
-            Snake snake = new Snake(p1, 10, Direction.RIGHT);
+            Snake snake = new Snake(p1, 4, Direction.RIGHT);
             snake.draw();
 
-
+            //Create random food 
+            FoodCreator foodCreator = new FoodCreator(91,22, 'o');
+            Point food = foodCreator.CreateFood();
+            food.draw();
 
             while (true) {
+             
+                snake.Move();
+                Thread.Sleep(100);
                 if (Console.KeyAvailable)
                 {
-                    ConsoleKeyInfo key = Console.ReadKey();
+                    ConsoleKeyInfo key = Console.ReadKey(true);
                     snake.handleKey(key.Key);
                 }
                 snake.Move();
                 Thread.Sleep(100);
+
+                if (snake.eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.draw();
+                }
+                else
+                {
+                    snake.Move();
+                    Thread.Sleep(100);
+                }
+
+               
             }
         }
 
